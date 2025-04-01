2fb1829a77ea85481e76a7f496627964f086a2aa
Irma Rrushi <irma0906@hotmail.com>
2025-04-01 19:44:20 -0400

    Resnet50

--- resnet50.py
+++ resnet50.py
@@ -8,6 +8,7 @@ class ResNet50(nn.Module):
     def __init__(self, num_classes=80):
         super(ResNet50, self).__init__()
         self.resnet_model = nn.Sequential(
+
             # Initial convolutional layer
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False), 
             nn.BatchNorm2d(64),
@@ -37,9 +38,15 @@ class ResNet50(nn.Module):
             Block(1024, 512, 2048, stride=2),
             Block(2048, 512, 2048, stride=1),
             Block(2048, 512, 2048, stride=1),
+
+            # Average Pooling and FC Layer
+            nn.AdaptiveAvgPool2d((1, 1)), # Reduce each channel to target size(1,1)
+            nn.Flatten(),
+            nn.Linear(2048, 80)    # 80 classes for COCO dataset
             
         )
 
+    # Forward pass
     def forward(self, x):
         return self.resnet_model(x)
 
