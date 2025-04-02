240b38c28b76fcfb195a1c3101f0e3c11b1c10f0
wiIliu <willowconnelly@oakland.edu>
2025-04-01 20:28:37 -0400

    Updated model and benchmarking values

--- modelTraining.py
+++ modelTraining.py
@@ -2,13 +2,13 @@ from dataPreprocessLoader import trainDataloader
 import torch.nn as nn
 from torch.optim import SGD, Adam
 import torch
-from torchvision.models import resnet50
+from resnet50 import ResNet50
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = ResNet50(num_classes=80)
 
-# TESTING WITH A TEMPORARY RESNET MODEL
-model = resnet50()
-
+# during validation stage, modify these hyperparameters
 optimizer = SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
 # optimizer = Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
 loss_fn = nn.CrossEntropyLoss()
@@ -19,6 +19,8 @@ def trainModel(dataloader, model, optimizer, loss_fn, epochs):
     for epoch in range(epochs):
         model.train()
         lossVal = 0
+        numCorrect = 0
+        total = 0
 
         for imgs, lbls in dataloader:
             imgs.to(device)
@@ -31,9 +33,12 @@ def trainModel(dataloader, model, optimizer, loss_fn, epochs):
             optimizer.step()
             lossVal += loss.item()
             preds = torch.argmax(preds, dim=1)
-
+            numCorrect += torch.sum(preds == lbls).item()
+            total+= len(lbls)
 
         print(f"Epoch {epoch+1} - Loss: {lossVal/len(dataloader):.4f}")
+        accuracy = numCorrect / total * 100
+        print(f"Epoch {epoch+1} - Accuracy: {accuracy:.4f}")
 
 
 trainModel(trainDataloader, model, loss_fn, optimizer,epochs)
