ad144d18bf8723d5bec5b016a1733e3c77656fa3
Willow Connelly <123427248+wiIliu@users.noreply.github.com>
2025-04-18 18:44:50 -0400

    Rename compileMode.sh to compileModel.sh

--- src/compileMode.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-vai_c_xir -x quantize_result/ResNet50_int.xmodel -a arch.json -o out_dpu -n resnet50_pytorch
---
+++ src/compileModel.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+vai_c_xir -x quantize_result/ResNet50_int.xmodel -a arch.json -o out_dpu -n resnet50_pytorch
