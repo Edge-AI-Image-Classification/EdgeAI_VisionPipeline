bf7a91151c146fe66475670be9beba077db7d12b
Shreyas Renukuntla <shreyas.ren@gmail.com>
2025-04-21 15:11:34 -0400

    Minor updates

--- src/cudachecker.py
+++
@@ -1,4 +0,0 @@
-import torch
-print(torch.cuda.is_available())        # Should be True
-print(torch.version.cuda)               # Should say 12.1
-print(torch.cuda.get_device_name(0))    # Should say "NVIDIA GeForce RTX 5080"
